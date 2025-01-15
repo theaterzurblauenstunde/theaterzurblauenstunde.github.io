874f0d7142d737d3f52ab88c042b129585971436
Hylian-a6324 <30595692+Hylian-a6324@users.noreply.github.com>
2025-01-15 13:10:17 +0100

    Header und Footer über Javascript auf Startseite laden

---
+++ main.js
@@ -0,0 +1,10 @@
+// Lädt Header und Footer
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => document.querySelector('header').innerHTML = data);
+
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => document.querySelector('footer').innerHTML = data);
+});
